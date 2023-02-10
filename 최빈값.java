6fdd6a9c4baae6d63cbdf62e67816230f2f3d5f8
Donggyeong <ehdrud1129@naver.com>
2023-02-10 11:52:03 +0900

    Create 최빈값.java

---
+++ 최빈값.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        
+        for(int el : array){
+            int mapValue = map.getOrDefault(el, 0);
+            map.put(el, mapValue + 1);
+        }
+        
+        List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(map.entrySet());
+        entryList.sort(Map.Entry.comparingByValue());
+        for(Map.Entry<Integer, Integer> entry : entryList){
+            System.out.println("key : " + entry.getKey() + ", value : " + entry.getValue());
+        }
+        
+        return answer;
+    }
+}
