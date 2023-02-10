dfe3f1e3b1e77b9931b51397e85a4c6002850aca
Donggyeong <ehdrud1129@naver.com>
2023-02-10 11:55:21 +0900

    Update 최빈값.java

--- 최빈값.java
+++ 최빈값.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
-class Solution {
-    public void solution(int[] array) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
-        
-        for(int el : array){
-            int mapValue = map.getOrDefault(el, 0);
-            map.put(el, mapValue + 1);
-        }
-        
-        List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(map.entrySet());
-        entryList.sort(Map.Entry.comparingByValue());
-        for(Map.Entry<Integer, Integer> entry : entryList){
-            System.out.println("key : " + entry.getKey() + ", value : " + entry.getValue());
-        }
+public List<Map.Entry<Integer, Integer>> getModeValue(int[] array) {
+    HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+    for(int el : array){
+        int mapValue = map.getOrDefault(el, 0);
+        map.put(el, mapValue + 1);
     }
+
+    List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(map.entrySet());
+    entryList.sort(Map.Entry.comparingByValue());
+    
+    for(Map.Entry<Integer, Integer> entry : entryList){
+        System.out.println("key : " + entry.getKey() + ", value : " + entry.getValue());
+    }
+    
+    return entryList;
 }
