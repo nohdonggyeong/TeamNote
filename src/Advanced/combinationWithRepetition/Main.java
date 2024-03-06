b5d7b2357980ba329f898bf340a7066be2496fa2
MartinNoh <ehdrud1129@naver.com>
2024-03-06 23:08:28 +0900

    순열과 조합 템플릿 작성
    Signed-off-by: MartinNoh <ehdrud1129@naver.com>

---
+++ src/Advanced/combinationWithRepetition/Main.java
@@ -0,0 +1,64 @@
+package Advanced.combinationWithRepetition;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, r;
+	static int[] input,temp;
+	static List<int[]> output;
+	
+	static void combinationWithRepetition(int start, int depth) {
+		if (depth == r) {
+			output.add(temp.clone());
+			return;
+		}
+		
+		for (int i = start; i < n; i++) {
+			temp[depth] = input[i];
+			combinationWithRepetition(i, depth + 1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+			StringTokenizer st;
+			StringBuilder sb = new StringBuilder();
+			
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			r = Integer.parseInt(st.nextToken());
+			
+			input = new int[n];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < n; i++) {
+				input[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			temp = new int[r];
+			output = new ArrayList<>();
+			combinationWithRepetition(0, 0);
+			
+			for (int[] el : output) {
+				for (int e : el) {
+					sb.append(e).append(" ");
+				}
+				sb.deleteCharAt(sb.length() - 1);
+				sb.append("\n");
+			}
+			
+			bw.write(sb.toString().trim());
+			bw.flush();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
